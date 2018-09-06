70a0c4974e0bf5c3600197f014d5d01da8b8b8d4
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-05 19:45:58 -0700

    iudhia

---
+++ server.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var PORT = process.env.PORT || 8080;
+// support parsing of application/json type post data
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes.js")(app);
+
+
+
+
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function (){
+    console.log("App listening on PORT: " + PORT);
+});
+
