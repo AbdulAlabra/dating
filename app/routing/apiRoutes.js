4df4ddc57f9d88faae73329d6b48b1b9c584bedd
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-06 21:40:20 -0700

    before heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,24 @@
 
 var friends = require("../data/friends.js")
 
-module.exports = function(app) {
-app.get("/api/friends", function (req, res) {
-res.json(friends);
-});
+// var match = friends.filter(function (obj) {
+//     var x;
+//     // if (obj.name === 'hhhh') {
+//     //     obj.scores.reduce(function (sum) {
+//     //          x =+ sum;
+//     //         return console.log(x);
+//     //     })
+//     // }
+// });
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        friends.push(req.body);
+        res.json(true);
+
+    });
 
 } // end of the func 
